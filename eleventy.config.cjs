8b0a44b9be7bf9989361dbfadafedcf412f0e764
Benny Powers <web@bennypowers.com>
2023-01-09 19:01:01 +0200

    fix: move site stuff to site config

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -2,6 +2,17 @@ const TokensPlugin = require('./plugins/11ty.cjs');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(TokensPlugin);
+
+  eleventyConfig.on('eleventy.before', async function() {
+    const { cp, mkdir } = await import('node:fs/promises');
+    const assetsDir = `${__dirname}/build/assets`;
+    await mkdir(assetsDir, { recursive: true });
+    await cp(`${__dirname}/css/global.css`, `${assetsDir}/rhds.css`);
+    await cp(`${__dirname}/plugins/11ty/styles.css`, `${assetsDir}/styles.css`);
+  });
+
+  eleventyConfig.addPassthroughCopy('docs/assets');
+
   return {
     htmlTemplateEngine: 'njk',
     dir: {
