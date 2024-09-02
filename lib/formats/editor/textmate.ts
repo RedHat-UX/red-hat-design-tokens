8ee935ca1d41732bf6fc7245bf7138abeb4236c4
Benny Powers <web@bennypowers.com>
2024-09-02 14:45:48 +0300

    fix: textmate comments

--- lib/formats/editor/textmate.ts
+++ lib/formats/editor/textmate.ts
@@ -11,7 +11,8 @@ import { isThemeColorToken } from '../../predicates.ts';
 export const textmateSnippets: Format = {
   name: 'editor/snippets/textmate',
   format: ({ dictionary }) =>
-    dictionary.allTokens.reduce((snippets, token) => `${snippets}
+    dictionary.allTokens.reduce((snippets, token) => `${snippets}${token.$description ? `
+# ${token.$description}` : ''}
 snippet ${token.name.replaceAll('-', '')}
   var(--${token.name}${isThemeColorToken(token) ? '' : `\${1:, ${token.$value}}`})$2${!token.$value?.startsWith?.('#') ? '' : `
 snippet ${token.$value.replace(/^#/, '')}
